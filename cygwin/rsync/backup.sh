4463ff64a352cc71e4daae9b8d7def1d6e786bc2
Laurent Weyand <laurent.weyand-prestataire@lcl.fr>
2019-08-27 14:25:02 +0200

    fix(rsync): enhance backup script to skip user and group id

--- cygwin/rsync/backup.sh
+++ cygwin/rsync/backup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+set -o errexit
 set -o nounset
 
 BASE_PATH="$(dirname ${0})"
@@ -10,18 +11,25 @@ LOG_FILE="/var/log/backup.log"
 
 source ${BASE_PATH}/local-setup.sh
 
-if [[ -e ${EXCLUDE_FILE} ]]; then
+function backup() {
+    local -r source_dir=$1
+    local -r target_dir=$2
+
     test=''
     if [[ ${DEBUG} == 'true' ]]; then
         echo 'Debug mode enabled'
+        echo "backup ${source_dir} ${target_dir}"
         test='--dry-run'
     fi
 
-    # Sauvegarde réoertoire de travail
-    rsync -var --exclude-from=${EXCLUDE_FILE} --delete --human-readable ${test} --log-file=${LOG_FILE} "${BASE_ORIGIN}${WORK_ORIGIN}/" "${BASE_TARGET}${WORK_TARGET}/"
-    #e=$?
-    # Sauvegarde réoertoire home utilisateur
-    rsync -var --exclude-from=${EXCLUDE_FILE} --delete --human-readable ${test} --log-file=${LOG_FILE} "${BASE_ORIGIN}${HOME_ORIGIN}/" "${BASE_TARGET}${HOME_TARGET}/"
+    rsync -rltDvWxP --exclude-from=${EXCLUDE_FILE} --delete --human-readable ${test} --log-file=${LOG_FILE} "${source_dir}" "${target_dir}"
+}
+
+if [[ -e ${EXCLUDE_FILE} ]]; then
+    # Sauvegarde répertoire de travail
+    backup "${BASE_ORIGIN}${WORK_ORIGIN}/" "${BASE_TARGET}${WORK_TARGET}/"
+    # Sauvegarde répertoire home utilisateur
+    backup  "${BASE_ORIGIN}${HOME_ORIGIN}/" "${BASE_TARGET}${HOME_TARGET}/"
 else
     echo "No ${EXCLUDE_FILE} file found"
 fi
--- cygwin/rsync/restore.sh
+++ cygwin/rsync/restore.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+set -o errexit
 set -o nounset
 
 BASE_PATH="$(dirname ${0})"
@@ -8,14 +9,22 @@ LOG_FILE="/var/log/restore.log"
 
 source ${BASE_PATH}/local-setup.sh
 
-test=''
-if [[ ${DEBUG} == 'true' ]]; then
-    echo 'Debug mode enabled'
-    test='--dry-run'
-fi
+function restore() {
+    local -r source_dir=$1
+    local -r target_dir=$2
 
-# Restaure réoertoire Data
-rsync -var --human-readable ${test} --log-file=${LOG_FILE} "${BASE_TARGET}${WORK_TARGET}/" "${BASE_ORIGIN}${WORK_ORIGIN}/"
+    test=''
+    if [[ ${DEBUG} == 'true' ]]; then
+        echo 'Debug mode enabled'
+        echo "restore ${source_dir} ${target_dir}"
+        test='--dry-run'
+    fi
+
+    rsync -rltDvWxP --human-readable ${test} --log-file=${LOG_FILE} "${source_dir}" "${target_dir}"
+}
+
+# Restaure répertoire Data
+restore "${BASE_TARGET}${WORK_TARGET}/" "${BASE_ORIGIN}${WORK_ORIGIN}/"
 #e=$?
-# Restaure réoertoire home
-rsync -var --human-readable ${test} --log-file=${LOG_FILE} "${BASE_TARGET}${HOME_TARGET}/" "${BASE_ORIGIN}${HOME_ORIGIN}/"
+# Restaure répertoire home
+restore "${BASE_TARGET}${HOME_TARGET}/" "${BASE_ORIGIN}${HOME_ORIGIN}/"
