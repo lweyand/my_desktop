5cce527915355c965e5e0b9badbd88dc75205d84
Laurent Weyand <laurent.weyand-prestataire@lcl.fr>
2019-11-13 11:53:26 +0100

    chore: enhance cygwin batch after another testing

--- windows/cygwin/configure_home_cygwin.bat
+++ windows/cygwin/configure_home_cygwin.bat
@@ -1,7 +1,6 @@
 rem Script DOS pour installer et configurer cygwin
 
 set USER=weyand_l
-set CYGWIN_PATH=C:\Users\%USER%\Downloads
 set CYGWIN_EXE=setup-x86_64.exe
 set CYGWIN_ROOT=C:\cygwin64
 
--- windows/cygwin/install_cygwin.bat
+++ windows/cygwin/install_cygwin.bat
@@ -2,6 +2,7 @@ rem Script DOS pour installer et configurer cygwin
 
 set USER=weyand_l
 set CYGWIN_PATH=C:\Users\%USER%\Downloads
+set CYGWIN_REPO=%CYGWIN_PATH%
 set CYGWIN_EXE=setup-x86_64.exe
 set CYGWIN_ROOT=C:\cygwin64
 
@@ -9,9 +10,10 @@ rem --- Installation de cygwin ---
 
 %CYGWIN_PATH%\%CYGWIN_EXE% ^
 --quiet-mode ^
+--proxy 127.0.0.1:3129 ^
+--site http://mirror.easyname.at/cygwin/ ^
 --root "%CYGWIN_ROOT%" ^
 --arch x86_64 ^
---local-package-dir "%CYGWIN_PATH%" ^
+--local-package-dir "%CYGWIN_REPO%" ^
 --verbose ^
 --packages bash-completion,curl,dos2unix,git,grep,jq,less,nano,openssh,openssl,rsync,tmux,tree,wget,procps-ng
-
